639c3f1a6ff9fab240a99fe935a76cb7f2d3d706
Barfuin <43729983+barfuin@users.noreply.github.com>
2018-10-20 13:36:24 +0200

    Connect shufflejs for tag filter

--- js/pagemarks.js
+++ js/pagemarks.js
@@ -5,8 +5,30 @@ function pagemarksMain() {
     var Shuffle = window.Shuffle;
     var element = document.querySelector('.pagemarks-shuffle-container');
 
-    var shuffleInstance = new Shuffle(element, {
+    const shuffleInstance = new Shuffle(element, {
         itemSelector: '.pagemarks-item',
         sizer: '.pagemarks-shuffle-container:first-child'
     });
+
+    const inputForm = $('#pagemarks-filter');
+    inputForm.on('submit', null, shuffleInstance, updateFilter);
+}
+
+
+
+function updateFilter(event) {
+    event.preventDefault();
+    const s = getValueFromInput('pagemarks-filter input', Shuffle.ALL_ITEMS);
+    console.log("filter changed - " + s);
+    event.data.filter(s);
+    return false;
+}
+
+function getValueFromInput(pInputFieldName, pDefault) {
+    let result = pDefault;
+    const v = $('#' + pInputFieldName).val();
+    if (typeof (v) === 'string' && v.trim().length > 0) {
+        result = v.trim();
+    }
+    return result;
 }
